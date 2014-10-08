032be77e159f33f794e1a1304887245bb47703da
mustcode <mustcode@gmail.com>
2014-10-09 02:25:51 +0700

    auto gen description and comment file when creating tasks

--- addtask.py
+++ addtask.py
@@ -13,6 +13,10 @@ else:
     del sys.argv[0]
     taskName = ' '.join(sys.argv)
 
+if os.path.exists(tasks.path(taskName)):
+    print('error: task already exists.')
+    sys.exit()
+
 print('adding task: ' + taskName)
 taskPath = tasks.add(taskName)
 fields.add(taskPath, fields.CREATED_BY, users.current)
--- tasks.py
+++ tasks.py
@@ -22,9 +22,20 @@ WORKITEM = 'workitem'
 
 types = [FEATURE, IMPROVEMENT, BUG, WORKITEM]
 
-def add(taskName):
+def path(taskName):
     dirName = taskName.replace(' ', '_')
-    taskPath = settings.TASKS_DIR + os.sep + dirName
+    return settings.TASKS_DIR + os.sep + dirName
+
+def add(taskName):
+    taskPath = path(taskName)
     assert not os.path.exists(taskPath)
     os.makedirs(taskPath)
+    
+    descFile = open(taskPath + os.sep + 'description.txt', 'w')
+    descFile.write('write description for this task here...')
+    descFile.close()
+    
+    commentsFile = open(taskPath + os.sep + 'comments.txt', 'w')
+    commentsFile.close()
+
     return taskPath
