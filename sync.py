aad745704b3ac6a75afbbba533098d4fc2d641e8
mustcode <mustcode@gmail.com>
2014-10-10 03:58:23 +0700

    A  sync.py

---
+++ sync.py
@@ -0,0 +1,18 @@
+import subprocess
+
+def pull():
+    subprocess.call('git pull origin master')
+
+def commit():
+    changes = subprocess.getoutput('git status --short')
+    if not changes:
+        return
+    subprocess.call('git add .')
+    commitMsg = subprocess.getoutput('git status --short')
+    subprocess.call('git commit -m "{0}"'.format(commitMsg))
+
+def sync():
+    pull();
+    commit();
+
+sync()
