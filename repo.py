312d4db635fcbd1c82ce1daf8f5b3ba8ebe402f1
Maethee Chongchitnant <eddie@pocketplaylab.com>
2014-10-14 19:10:01 +0700

    usage: git [--version] [--help] [-C <path>] [-c name=value]            [--exec-path[=<path>]] [--html-path] [--man-path] [--info-path]            [-p|--paginate|--no-pager] [--no-replace-objects] [--bare]            [--git-dir=<path>] [--work-tree=<path>] [--namespace=<name>]            <command> [<args>]
    The most commonly used git commands are:
       add        Add file contents to the index
       bisect     Find by binary search the change that introduced a bug
       branch     List, create, or delete branches
       checkout   Checkout a branch or paths to the working tree
       clone      Clone a repository into a new directory
       commit     Record changes to the repository
       diff       Show changes between commits, commit and working tree, etc
       fetch      Download objects and refs from another repository
       grep       Print lines matching a pattern
       init       Create an empty Git repository or reinitialize an existing one
       log        Show commit logs
       merge      Join two or more development histories together
       mv         Move or rename a file, a directory, or a symlink
       pull       Fetch from and integrate with another repository or a local branch
       push       Update remote refs along with associated objects
       rebase     Forward-port local commits to the updated upstream head
       reset      Reset current HEAD to the specified state
       rm         Remove files from the working tree and from the index
       show       Show various types of objects
       status     Show the working tree status
       tag        Create, list, delete or verify a tag object signed with GPG
    
    'git help -a' and 'git help -g' lists available subcommands and some
    concept guides. See 'git help <command>' or 'git help <concept>'
    to read about a specific subcommand or concept.

--- repo.py
+++ repo.py
@@ -1,18 +1,18 @@
 import subprocess
 
 def pull():
-    subprocess.call('git pull origin master')
+    subprocess.call(['git', 'pull', 'origin', 'master'])
 
 def commit():
-    changes = subprocess.getoutput('git status --short')
+    changes = subprocess.getoutput(['git', 'status', '--short'])
     if not changes:
         return
-    subprocess.call('git add --all .')
-    commitMsg = subprocess.getoutput('git status --short')
-    subprocess.call('git commit -m "{0}"'.format(commitMsg))
+    subprocess.call(['git', 'add', '--all', '.'])
+    commitMsg = subprocess.getoutput(['git', 'status', '--short'])
+    subprocess.call(['git', 'commit', '-m', "{0}".format(commitMsg)])
 
 def push():
-    subprocess.call('git push -u origin master')
+    subprocess.call(['git', 'push', '-u', 'origin', 'master'])
 
 def sync():
     pull()
